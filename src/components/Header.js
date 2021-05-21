43ef1b1a6bd296af1eb159ecf54a8e6be54082b5
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-21 21:46:46 +0530

    working on the redux part <cart>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
 import { signIn, signOut, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
 
 const Header = () => {
   const [session] = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
+
     return (
       <header className="">
         <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
           <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
             <Image
+              onClick={() => router.push("/")}
               src="https://links.papareact.com/f90"
               width={150}
               height={40}
@@ -27,7 +34,7 @@ const Header = () => {
 
           <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
             <div onClick={!session ? signIn : signOut} className="link">
-              <p>Hello, {session ? `${session.user.name}`:'Sign In'}</p>
+              <p>Hello, {session ? `${session.user.name}` : "Sign In"}</p>
               <p className="font-extrabold md:text-sm">Account & Lists</p>
             </div>
 
@@ -36,9 +43,12 @@ const Header = () => {
               <p className="font-extrabold md:text-sm">& Orders</p>
             </div>
 
-            <div className="relative link flex items-center">
+            <div
+              onClick={() => router.push("/checkout")}
+              className="relative link flex items-center"
+            >
               <span className="absolute top-0 right-0 md:right-7 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-                0
+                {items.length}
               </span>
               <ShoppingCartIcon className="h-10" />
               <p className="hidden md:inline font-extrabold md:text-sm mt-2">
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,13 +2,29 @@ import Image from 'next/image';
 import React, {useState} from 'react';
 import { StarIcon } from '@heroicons/react/solid';
 import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+import { addToBasket } from '../slices/basketSlice';
 
 const MIN_RATING = 1;
 const MAX_RATING = 5;
 
 const Product = ({ title, id, price, description, category, image }) => {
     const [rating] = useState(Math.floor(Math.random()*(MAX_RATING-MIN_RATING + 1))+ MIN_RATING);
-    const [hasPrime] = useState(Math.random() < 0.5 );
+    const [hasPrime] = useState(Math.random() < 0.5);
+    const dispatch = useDispatch();
+
+    const addItemToCart = () => {
+        const product = {
+          title,
+          id,
+          price,
+          description,
+          category,
+          image,
+        };
+        //sending the product as an action to redux store!
+        dispatch(addToBasket(product));
+    };
 
     return <div className="relative flex flex-col m-5 bg-white z-30 p-10">
         <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
@@ -34,7 +50,7 @@ const Product = ({ title, id, price, description, category, image }) => {
             </div>
         )}
 
-        <button className="mt-auto button">Add to Cart</button>
+        <button onClick={addItemToCart} className="mt-auto button">Add to Cart</button>
   </div>;
 };
 
